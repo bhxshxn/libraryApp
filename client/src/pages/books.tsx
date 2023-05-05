c89eae8bc32e4f720839dcf3d46058d886251596
Yash-Simplify <yash.jayant@simplifytech.in>
2023-05-05 12:09:55 +0530

    issue

--- client/src/pages/books.tsx
+++ client/src/pages/books.tsx
@@ -1,48 +1,48 @@
-import { Book, getBooks } from '@/api/getBook';
-import Header from '@/components/Header';
-import FullScreenLoader from '@/components/FullScreenLoader';
-import MobileListItem from '@/components/MobileListItem';
-import styles from '@/styles/Home.module.css';
-import { useState } from 'react';
-import { useQuery } from 'react-query';
-import Loader from '@/components/Loader';
+import { Book, getBooks } from "@/api/getBook";
+import Header from "@/components/Header";
+import FullScreenLoader from "@/components/FullScreenLoader";
+import MobileListItem from "@/components/MobileListItem";
+import styles from "@/styles/Home.module.css";
+import { useState } from "react";
+import { useQuery } from "react-query";
+import Loader from "@/components/Loader";
 
 const Books = () => {
- const [search, setSearch] = useState('');
- const {
-  isLoading,
-  data: books,
-  error: booksError,
- } = useQuery(['books', search], () => getBooks(search));
+  const [search, setSearch] = useState("");
+  const {
+    isLoading,
+    data: books,
+    error: booksError,
+  } = useQuery(["books", search], () => getBooks(search));
 
- return (
-  <>
-   <Header />
-   <main className={styles.booksContainer}>
-    <input
-     className={styles.searchBar}
-     placeholder="Search for Book using Title"
-     onChange={(e) => {
-      setSearch(e.target.value);
-     }}
-     value={search}
-    />
-    {isLoading ? (
-     <Loader />
-    ) : (
-     books.map((item: Book) => (
-      <MobileListItem
-       id={item._id}
-       author={item.author}
-       title={item.title}
-       isbn={item.accNo}
-       key={item._id}
-      />
-     ))
-    )}
-   </main>
-  </>
- );
+  return (
+    <>
+      <Header />
+      <main className={styles.booksContainer}>
+        <input
+          className={styles.searchBar}
+          placeholder="Search for Book using Title"
+          onChange={(e) => {
+            setSearch(e.target.value);
+          }}
+          value={search}
+        />
+        {isLoading ? (
+          <Loader />
+        ) : (
+          books.map((item: Book) => (
+            <MobileListItem
+              id={item._id}
+              author={item.author}
+              title={item.title}
+              isbn={item.accNo}
+              key={item._id}
+            />
+          ))
+        )}
+      </main>
+    </>
+  );
 };
 
 export default Books;
--- server/routes/admin.js
+++ server/routes/admin.js
@@ -1,24 +1,57 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const issueModel = require('../models/issues');
+const issueModel = require("../models/issues");
 
 // index get
-router.post('/login', (req, res) => {
-   if (req.body.username == 'admin' && req.body.password == '123456') {
-      res.send({ token: 'admin@123' }).status(201);
-   } else {
-      res.send({ msg: 'username or password is wrong' }).status(401);
-   }
+router.post("/login", (req, res) => {
+  if (req.body.username == "admin" && req.body.password == "123456") {
+    res.send({ token: "admin@123" }).status(201);
+  } else {
+    res.send({ msg: "username or password is wrong" }).status(401);
+  }
 });
 
-router.post('/issueBook', (req, res) => {
-   const { issuerName, issueDate, issueStatus, returnDate } = req.body;
-   const issueData = new issueModel({
-      issuerName,
-      issueDate,
-      issueStatus,
-      returnDate,
-   });
+router.post("/issueBook", (req, res) => {
+  const { issuerName, issueDate, issueStatus, returnDate } = req.body;
+  const issueData = new issueModel({
+    issuerName,
+    issueDate,
+    issueStatus: true,
+  });
+
+  issueData
+    .save()
+    .then((response) => {
+      res.send({ msg: "Issued sucessfully", data: response }).status(200);
+    })
+    .catch((err) => {
+      console.log(err);
+      res.send({ msg: "err", err: err }).status(500);
+    });
+});
+
+router.post("/returnBook", (req, res) => {
+  const { id } = req.body;
+  // const issueData = new issueModel({
+  //   issuerName,
+  //   issueDate,
+  //   issueStatus: true,
+  // });
+  issueModel
+    .findByIdAndUpdate(
+      { _id: id },
+      {
+        issueStatus: false,
+        returnDate: "5/5/23",
+      }
+    )
+    .then((response) => {
+      res.send({ msg: "returned sucessfully", data: response }).status(200);
+    })
+    .catch((err) => {
+      console.log(err);
+      res.send({ msg: "err", err: err }).status(500);
+    });
 });
 
 module.exports = router;
